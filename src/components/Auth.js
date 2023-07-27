26c9cf866c91f5f0594a1977945ba87099addb89
Mohnish <mohnish.a007@gmail.com>
2023-07-27 09:47:11 +0100

    Applying dispatch methods in app

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -1,12 +1,19 @@
 import React from "react";
 
 import "./Auth.css";
+import { useDispatch } from "react-redux";
+import { authActions } from "../store/auth-slice";
 
 const Auth = () => {
+  const dispatch = useDispatch()
+  const handleSubmit = (event) =>{
+    event.preventDefault()  //Preventing default rerendering
+    dispatch(authActions.login())  //dispatch funcion activated
+  }
   return (
     <div className="container">
-      <h1>Login</h1>{" "}
-      <form>
+      <h1>Login</h1>
+      <form onSubmit={handleSubmit}>
         <label htmlFor="id">Id</label>
         <input type="text" name="id" id="id" />
         <label htmlFor="password">Password</label>
