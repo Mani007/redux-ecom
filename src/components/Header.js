60411ac6c47f93e82ee4b8821ac33293ec234949
Mohnish <mohnish.a007@gmail.com>
2023-07-28 14:46:31 +0100

    Totalquantity in the cart resolved.

--- src/components/Header.js
+++ src/components/Header.js
@@ -11,7 +11,7 @@ const Header = () => {
               className="header-h2"
               style={{ fontFamily: "monospace", fontSize: "30px" }}
             >
-              Redux Shopping App
+              Redux Ecomm App
             </h2>
           </li>
           <li>
