77dc5cfee840fff2a29f45558be0667a97fc8342
Mohnish <mohnish.a007@gmail.com>
2023-07-29 21:48:35 +0100

    cart remove item handler and reducer implemented

--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -26,6 +26,14 @@ const cartSlice = createSlice({
             }
         },
         removeFromCart(state,action){
+            const id = action.payload;
+            const existingItem = state.itemsList.find((item) => item.id === id);
+            if (existingItem.quantity === 1) {
+                state.itemsList = state.itemsList.filter((item) => item.id !== id);
+            } else {
+                existingItem.quantity--; 
+                existingItem.totalPrice -= existingItem.price;
+            }
 
         },
         setShowCart(state){
